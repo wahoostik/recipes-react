29c6da9f4604fae1956b9d1851128895029afd3d
wahoostik <anthonypaccaud@wanadoo.fr>
2021-06-03 18:34:26 +0200

    rajout des données

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,40 @@
-// == Import npm
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import Menu from 'src/components/Menu';
+import Home from 'src/components/Home';
+// import Recipe from 'src/components/Recipe';
+// import Error from 'src/components/Error';
 
-// == Composant
-const App = () => (
-  <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
-  </div>
-);
+import recipesTest from 'src/data';
 
-// == Export
+import Loading from './Loading';
+
+import './style.scss';
+
+function App(props) {
+  const recipes = {
+    list: recipesTest,
+  };
+  if (props.loading) {
+    return <Loading />;
+  }
+  return (
+    <div className="app">
+      <Menu recipes={recipes} />
+      <Home recipes={recipes} />
+      {/* <Recipe recipe={recipesTest[0]} /> */}
+      {/* <Error /> */}
+    </div>
+  );
+}
+
+App.propTypes = {
+  loading: PropTypes.bool,
+};
+
+App.defaultProps = {
+  loading: false,
+};
+
 export default App;
