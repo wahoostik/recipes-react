abdb9ff2c6844149756d95f91a04b5575c336b16
wahoostik <anthonypaccaud@wanadoo.fr>
2021-06-04 12:17:37 +0200

    Link du composant Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import './style.scss';
 
@@ -14,7 +15,7 @@ const Card = ({
     <div className="card-content">
       <h2 className="card-title">{title}</h2>
       <p className="card-desc">Difficulté : {difficulty}</p>
-      <a href={`/recipe/${slug}`} className="card-link">Voir la recette</a>
+      <Link to={`/recipe/${slug}`} className="card-link">Voir la recette</Link>
     </div>
   </article>
 );
