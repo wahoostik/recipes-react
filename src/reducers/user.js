ce088ee04ce3c932bb66f3c1bd0abecd92244aae
wahoostik <anthonypaccaud@wanadoo.fr>
2021-06-10 14:47:54 +0200

    traitement du case CHANGE VALUE dans le reducer user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,13 +1,26 @@
+/* eslint-disable max-len */
+/* eslint-disable no-console */
+import { CHANGE_VALUE } from 'src/actions/userActions';
+
 const initialState = {
   email: 'rudy.gobert@nba.com',
   password: 'utahjazz',
-  pseudo: '',
+  infos: {
+    pseudo: '',
+  },
 };
 
 // Le reducer doit toujours retourner le state
 const userReducer = (state = initialState, action = {}) => {
 // On switch sur le type d'action
   switch (action.type) {
+    case CHANGE_VALUE: {
+      console.log('case', CHANGE_VALUE);
+      return {
+        ...state,
+        [action.name]: action.value, // modification dynamique, ça sera soit email, soit password qui sera changé
+      };
+    }
     default:
       return state;
   }
