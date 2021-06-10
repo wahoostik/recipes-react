34a025faa7acd9e4dab9cfcf1c9f32d4a41dde5d
wahoostik <anthonypaccaud@wanadoo.fr>
2021-06-10 17:04:11 +0200

    traitement de l'action saveUser dans le reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 /* eslint-disable no-console */
-import { CHANGE_VALUE } from 'src/actions/userActions';
+import { CHANGE_VALUE, SAVE_USER } from 'src/actions/userActions';
 
 const initialState = {
   email: 'rudy.gobert@nba.com',
@@ -15,10 +15,21 @@ const userReducer = (state = initialState, action = {}) => {
 // On switch sur le type d'action
   switch (action.type) {
     case CHANGE_VALUE: {
-      console.log('case', CHANGE_VALUE);
+      // console.log('case', CHANGE_VALUE);
       return {
         ...state,
         [action.name]: action.value, // modification dynamique, ça sera soit email, soit password qui sera changé
+      };
+    }
+    case SAVE_USER: {
+      return {
+        ...state,
+        email: '',
+        password: '',
+        infos: {
+          ...state.infos,
+          pseudo: action.user.pseudo,
+        },
       };
     }
     default:
