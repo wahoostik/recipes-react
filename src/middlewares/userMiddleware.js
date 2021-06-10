52ec1017be044b4fc46d43a8af4b6ece4329d1e1
wahoostik <anthonypaccaud@wanadoo.fr>
2021-06-10 16:02:46 +0200

    traitement de la requête dans userMiddleware

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-console */
+import axios from 'axios';
 import { LOGIN } from 'src/actions/userActions';
 
-const userMiddleware = (store) => (next) => (action) => {
-  console.log('state:', store.getState());
-  console.log('action:', action);
+const userMiddleware = (store) => (next) => async (action) => {
+  // console.log('state:', store.getState());
+  // console.log('action:', action);
   switch (action.type) {
     case LOGIN: {
       console.log('case', LOGIN);
+      try {
+        const state = store.getState();
+        const response = await axios.post('http://localhost:3001/login', {
+          email: state.userReducer.email,
+          password: state.userReducer.password,
+        });
+        console.log('reponse', response);
+      }
+      catch (error) {
+        console.trace(error);
+      }
       break;
     }
     default:
